03a561d5458e0ad187189c61d2f61e0dd903fe86
lkvitsinadze <l.kvitsinadze@gmail.com>
2023-07-02 22:00:13 +0400

    Footer

--- templates/src/index.php
+++ templates/src/index.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 $root_path = $_SERVER['DOCUMENT_ROOT'];
-include($root_path . '/partials/head.php'); 
+include($root_path . '/partials/head.php');
 ?>
 
 <nav class="navigation" data-component='{ "component": "navigation" }'>
@@ -22,11 +22,11 @@ include($root_path . '/partials/head.php');
 						<a class="navigation__links__item label color-light" href="#">Crew</a>
 					</li>
 				</ul>
-				
+
 				<?php echo generate_component_button([
 					'label' => 'Contact',
 					'class' => 'navigation__button',
-					'icon' => 'arrow-right',
+					'f' => 'arrow-right',
 					'icon_type' => 'symbol',
 					'icon_right' => true,
 				]) ?>
@@ -69,11 +69,11 @@ include($root_path . '/partials/head.php');
 	</div>
 	<div class="container">
 		<div class="hero__teasers">
-			<?php for($i = 0; $i < 3; $i++) {
+			<?php for ($i = 0; $i < 3; $i++) {
 				$style = null;
 				$illustration = null;
-				if($i == 1) $style = 'black';
-				if($i == 2) $illustration = '/assets/images/map.svg';
+				if ($i == 1) $style = 'black';
+				if ($i == 2) $illustration = '/assets/images/map.svg';
 				echo generate_component_teaser([
 					'style' => $style,
 					'illustration' => $illustration,
@@ -110,7 +110,7 @@ include($root_path . '/partials/head.php');
 			</div>
 			<div class="text-content__text copy">
 				Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-				Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+				Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 			</div>
 		</div>
 	</div>
@@ -134,4 +134,64 @@ include($root_path . '/partials/head.php');
 	</div>
 </section>
 
-<?php include($root_path . '/partials/foot.php'); ?>  
+<footer class="footer">
+	<div class="container">
+		<div class="footer__inner">
+			<div class="footer__left">
+				<a href="#" class="">
+					<img class="" src="/assets/images/logo.svg" alt="Segelteam">
+				</a>
+				<p class="color-light copy">© 2021. Segel-Team. Alle Rechte vorbehalten</p>
+			</div>
+			<div class="footer__right">
+				<ul class="site-map">
+					<li><a href="#" class="site-map__item color-light">About us</a></li>
+					<li><a href="#" class="site-map__item color-light">Gallery</a></li>
+					<li><a href="#" class="site-map__item color-light">Crew</a></li>
+					<li><a href="#" class="site-map__item color-light">Contact us</a></li>
+					<li><a href="#" class="site-map__item color-light">Impressum</a></li>
+					<li><a href="#" class="site-map__item color-light">Datenschutz</a></li>
+					<li><a href="#" class="site-map__item color-light">Rechtliches</a></li>
+					<li><a href="#" class="site-map__item color-light">Copyright</a></li>
+				</ul>
+				<ul class="social-media">
+					<li>
+						<a href="" class="social-media__item">
+							<?php echo generate_component_icon([
+								'name' => 'facebook',
+								'type' => 'symbol'
+							]); ?>
+						</a>
+					</li>
+					<li>
+						<a href="" class="social-media__item">
+							<?php echo generate_component_icon([
+								'name' => 'twitter',
+								'type' => 'symbol'
+							]); ?>
+						</a>
+					</li>
+					<li>
+						<a href="" class="social-media__item">
+							<?php echo generate_component_icon([
+								'name' => 'instagram',
+								'type' => 'symbol'
+							]); ?>
+						</a>
+					</li>
+					<li>
+						<a href="" class="social-media__item">
+							<?php echo generate_component_icon([
+								'name' => 'youtube',
+								'type' => 'symbol'
+							]); ?>
+						</a>
+					</li>
+				</ul>
+			</div>
+
+		</div>
+	</div>
+</footer>
+
+<?php include($root_path . '/partials/foot.php'); ?>
