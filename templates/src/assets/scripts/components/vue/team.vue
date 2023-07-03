b8f044c052619ba08adca1bbe7e93c3c0b921260
lkvitsinadze <l.kvitsinadze@gmail.com>
2023-07-03 16:38:29 +0400

    Refactor team filtering

--- templates/src/assets/scripts/components/vue/team.vue
+++ templates/src/assets/scripts/components/vue/team.vue
@@ -61,6 +61,24 @@ export default {
 				isLoading: false,
 			},
 			itemWidth: '20%',
+			duties: [
+				{
+					title: 'Show all',
+					value: '',
+				},
+				{
+					title: 'Trim',
+					value: 'trim',
+				},
+				{
+					title: 'tactic',
+					value: 'tactic',
+				},
+				{
+					title: 'helmsman',
+					value: 'helmsman',
+				},
+			],
 		};
 	},
 };
@@ -74,25 +92,10 @@ export default {
 				<h5 class="h5 text-content__headline color-light">Subtitle goes here</h5>
 			</div>
 			<ul class="team-filter">
-				<li>
-					<a href="#" @click.prevent="filterByDuty('')"
+				<li v-for="duty in duties" :key="duty.value">
+					<a href="#" @click.prevent="filterByDuty(duty.value)"
 					class="team-filter__item"
-					:class="{'active': meta.duty == ''}">Show all</a>
-				</li>
-				<li>
-					<a href="#" @click.prevent="filterByDuty('trim')"
-					class="team-filter__item"
-					:class="{'active': meta.duty == 'trim'}">Trim</a>
-				</li>
-				<li>
-					<a href="#" @click.prevent="filterByDuty('tactic')"
-					class="team-filter__item"
-					:class="{'active': meta.duty == 'tactic'}">Tactic</a>
-				</li>
-				<li>
-					<a href="#" @click.prevent="filterByDuty('helmsman')"
-					class="team-filter__item"
-					:class="{'active': meta.duty == 'helmsman'}">Helmsman</a>
+					:class="{'active': meta.duty == duty.value}">{{duty.title}}</a>
 				</li>
 			</ul>
 		</div>
@@ -108,9 +111,12 @@ export default {
             </div>
 		</div>
 		<div class="team-pagination">
-            <a href="#" @click.prevent="updateGridSize(4)" class="team-pagination__item" :class="{'active': meta.limit == '4'}">4</a>
-            <a href="#" @click.prevent="updateGridSize(5)" class="team-pagination__item" :class="{'active': meta.limit == '5'}">5</a>
-            <a href="#" @click.prevent="updateGridSize(6)" class="team-pagination__item" :class="{'active': meta.limit == '6'}">6</a>
+            <a href="#" 
+			v-for="size in [4, 5, 6]"
+			:key="size"
+			 @click.prevent="updateGridSize(size)"
+			class="team-pagination__item"
+			:class="{'active': meta.limit == size}">{{size}}</a>
 		</div>
         <button @click="loadMore" class="load-more color-light">Load more</button>
     </div>
